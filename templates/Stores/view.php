cf70684c2c64b1015fcca31d3dc91be69b0c98a8
Alejandro <agramosinfo@gmail.com>
2020-11-03 16:44:23 +0100

    Añado estructura html para generar el sidebar, el topbar, el content, etc en default y cargue todo de una, despues añado modelos nuevos, y enruto en el menú para poder acceder a otras rutas.

---
+++ templates/Stores/view.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Store $store
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Store'), ['action' => 'edit', $store->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Store'), ['action' => 'delete', $store->id], ['confirm' => __('Are you sure you want to delete # {0}?', $store->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Stores'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Store'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="stores view content">
+            <h3><?= h($store->name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Name') ?></th>
+                    <td><?= h($store->name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($store->id) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Created') ?></th>
+                    <td><?= h($store->created) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Modified') ?></th>
+                    <td><?= h($store->modified) ?></td>
+                </tr>
+            </table>
+            <div class="text">
+                <strong><?= __('Title') ?></strong>
+                <blockquote>
+                    <?= $this->Text->autoParagraph(h($store->title)); ?>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ tests/TestCase/Model/Table/StoresTableTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\StoresTable;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\StoresTable Test Case
+ */
+class StoresTableTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\StoresTable
+     */
+    protected $Stores;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'app.Stores',
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $config = $this->getTableLocator()->exists('Stores') ? [] : ['className' => StoresTable::class];
+        $this->Stores = $this->getTableLocator()->get('Stores', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        unset($this->Stores);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
